f54a5edece8b803def56b3ddb544c11d9bcfaf39
Mohamad <moh.dabboussi@gmail.com>
2024-05-24 16:58:34 +1000

    added new dist file

--- src/SearchPlugin.ts
+++ src/SearchPlugin.ts
@@ -1,25 +1,9 @@
 import { App } from "vue";
 
 import CrucibleSearch from "./components/CrucibleSearch.vue";
-import { PluginOptions } from "./types";
 
-export const defaultData = {
-  data: {
-    questions: [
-      {
-        _id: { $oid: "6625c7c8c8259deb8c3af39e" },
-        statement: "",
-        tags: [""],
-        optionsList: { optionValue: "", optionCorrect: false },
-        link: "",
-      },
-    ],
-  },
-};
-
-export function createSearchPlugin(app: App, options: PluginOptions = {}) {
+export function createSearchPlugin(app: App) {
   app.component("CrucibleSearch", CrucibleSearch);
-  app.provide("$dataLink", options.dataLink || defaultData);
 }
 
 export { CrucibleSearch };
