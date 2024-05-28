42df0c01e5788d06fcb5168aabb80a87b7b7ddec
Jo-Zh <joanna.zhong@uq.edu.au>
2024-05-28 20:52:36 +1000

    update injectRoute

--- src/router/injectRoute.ts
+++ src/router/injectRoute.ts
@@ -1,14 +1,10 @@
-// useRouter.ts
 import { inject } from "vue";
 import { Router } from "vue-router";
-import { router as ProjectRouter } from "@/router/projectRoutes";
-// const projectRouter = router;
 
 export function useRouter(): Router {
   const router = inject("$router") as Router;
   if (!router) {
-    //  throw new Error("Router instance is not provided");
-    return ProjectRouter;
+    throw new Error("Router instance is not provided");
   }
 
   return router;
